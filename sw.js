312e361d6866ea2b892b1c991c6ba18e9d4c8de4
zxcpoi7753-alt <zxcpoi7753@gmail.com>
2026-02-03 04:50:19 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE_NAME = 'thuraya-v6-offline';
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
