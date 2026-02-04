1a0212c71ad62cb4edf91336c82930747a24c1c9
zxcpoi7753-alt <zxcpoi7753@gmail.com>
2026-02-04 14:46:06 +0300

    Create AuthContext.jsx

---
+++ src/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
+    const logout = () => signOut(auth);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+        return unsubscribe;
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+};
